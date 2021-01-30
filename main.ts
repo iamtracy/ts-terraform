8e357edb20dfa0c6ef8ae31e6a11f54c4e0e2611
Tracy Lee <trlee@vestmark.com>
2021-01-30 14:40:52 -0600

    terraform deploys services

--- main.ts
+++ main.ts
@@ -1,39 +1,22 @@
-import { EcrRepository } from './.gen/providers/aws/ecr-repository';
+import { App, TerraformOutput, TerraformStack } from 'cdktf'
+import { Construct } from 'constructs'
+import { EcrRepository } from './.gen/providers/aws/ecr-repository'
 import { EcsService } from './.gen/providers/aws/ecs-service'
 import { EcsTaskDefinition } from './.gen/providers/aws/ecs-task-definition'
 import { EcsCluster } from './.gen/providers/aws/ecs-cluster'
-import { App, TerraformOutput, TerraformStack } from 'cdktf'
-import { Construct } from 'constructs'
 import { Instance } from './.gen/providers/aws/instance'
 import { AwsProvider } from './.gen/providers/aws/aws-provider'
-import { Container, Image } from "./.gen/providers/docker"
-import { DockerProvider } from './.gen/providers/docker'
 
-class Aws extends TerraformStack {
+class Stack extends TerraformStack {
 	constructor(scope: Construct, name: string) {
 		super(scope, name)
 		
-		new DockerProvider(this, 'default', {})
-		const { latest } = new Image(this, 'nginxImage', {
-			name: 'nginx:latest',
-			keepLocally: false,
-		})
-		const container = new Container(this, 'nginxContainer', {
-			image: latest,
-			name: 'tutorial',
-			ports: [
-				{
-					internal: 80,
-					external: 8000,
-				},
-			],
-		})
 		new EcrRepository(this, 'image', { name })
 		const cluster = new EcsCluster(this, 'cluster', { name })
 		new EcsTaskDefinition(this, 'task', { 
 			containerDefinitions: JSON.stringify([{
-				name: container.name,
-				image: container.image,
+				name: 'nginxImage',
+				image: 'sha256:f6d0b4767a6c466c178bf718f99bea0d3742b26679081e52dbf8e0c7c4c42d74',
 				memory: 1024,
 				ports: {
 					containerPort: 80,
@@ -64,5 +47,5 @@ class Aws extends TerraformStack {
 }
 
 const app = new App()
-new Aws(app, 'typescript-aws')
+new Stack(app, 'typescript-aws')
 app.synth()
