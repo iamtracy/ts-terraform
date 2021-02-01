4e837f2590d814f3e63cee191a84b0c102a60f4a
Tracy Lee <tlee@inxpo.com>
2021-01-31 22:18:14 -0600

    Feature/cra (#1)
    * terraform
    
    Co-authored-by: Tracy Lee <trlee@vestmark.com>
    Co-authored-by: iamtracy <iamtracy@users.noreply.github.com>

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ import { EcsTaskDefinition } from './.gen/providers/aws/ecs-task-definition'
 import { EcsCluster } from './.gen/providers/aws/ecs-cluster'
 import { Instance } from './.gen/providers/aws/instance'
 import { AwsProvider } from './.gen/providers/aws/aws-provider'
+import { containerDefinitions } from './task-definition.json'
 
 class Stack extends TerraformStack {
 	constructor(scope: Construct, name: string) {
@@ -14,16 +15,7 @@ class Stack extends TerraformStack {
 		new EcrRepository(this, 'image', { name })
 		const cluster = new EcsCluster(this, 'cluster', { name })
 		new EcsTaskDefinition(this, 'task', { 
-			containerDefinitions: JSON.stringify([{
-				name: 'nginxImage',
-				image: 'sha256:f6d0b4767a6c466c178bf718f99bea0d3742b26679081e52dbf8e0c7c4c42d74',
-				memory: 1024,
-				ports: {
-					containerPort: 80,
-					hostPort: 8000,
-				},
-				cpu: 2,
-			}] as Array<Partial<{}>>),
+			containerDefinitions: JSON.stringify(containerDefinitions),
 			family: name
 		})
 		new EcsService(this, 'service', { name, taskDefinition: name, cluster: cluster.id })
@@ -38,7 +30,7 @@ class Stack extends TerraformStack {
         fruit: 'blueberry',
         Address: '123 Main St',
       },
-    })
+		})
 
     new TerraformOutput(this, 'public_ip', {
       value: publicIp,
